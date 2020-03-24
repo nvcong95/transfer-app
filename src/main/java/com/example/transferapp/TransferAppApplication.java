0760314b097fbf3cf059aeee8a4a8c070c09238c
phamphuc1301 <phamphuc224@gmail.com>
2020-03-24 22:45:40 +0700

    fix url not found

--- src/main/java/com/example/transferapp/TransferAppApplication.java
+++ src/main/java/com/example/transferapp/TransferAppApplication.java
@@ -15,25 +15,6 @@ public class TransferAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TransferAppApplication.class, args);
-        List<BankAccount> bankAccounts = new ArrayList<>();
-
-        if (Optional.ofNullable(bankAccounts).isPresent()) {
-            List<BankAccount> account = Optional.ofNullable(bankAccounts).get();
-        }
-
-        SessionFactory sessionFactory = ConnectionUtil.getSessionFactory();
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.save(new BankAccount("Tom", 1000));
-            session.save(new BankAccount("Jerry", 2000));
-            session.save(new BankAccount("Donald", 3000));
-            session.getTransaction().commit();
-        }catch (Exception e){
-
-        }
-
-        sessionFactory.close();
     }
 
 }
--- src/main/java/com/example/transferapp/controller/MainController.java
+++ src/main/java/com/example/transferapp/controller/MainController.java
@@ -9,23 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1")
 public class MainController {
     @Autowired
     private BankAccountDAO bankAccountDAO;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String ShowBankAccounts(Model model) {
-        List<BankAccountInfo> list = bankAccountDAO.listBankAccountInfo();
-        model.addAttribute("accountInfos", list);
+//        List<BankAccountInfo> list = bankAccountDAO.listBankAccountInfo();
+//        model.addAttribute("accountInfos", list);
         return "sendMoneyPage";
     }
     @RequestMapping(value = "/sendMoney", method = RequestMethod.GET)
